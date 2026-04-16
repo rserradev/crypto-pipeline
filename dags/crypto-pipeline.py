7a0239c62d7ddb988dad6f0aaacc38505016b392
rserradev <ronaldoserra1998@gmail.com>
2026-04-15 21:01:28 -0400

    fix: remove unnecessary blank line in crypto-pipeline.py

--- dags/crypto-pipeline.py
+++ dags/crypto-pipeline.py
@@ -28,7 +28,6 @@ MINIO_CONN = {
 BUCKET = "crypto-bronze"
 COINS = ["bitcoin", "ethereum", "solana", "cardano"]
 
-
 def fetch_prices():
     # 1. Llamar a la API
     url = "https://api.coingecko.com/api/v3/coins/markets"
